598c236fdb6906515e44ed5cc24b7a5738a53357
Alex Hidalgo <aj1hidalgo@gmail.com>
2018-05-29 21:49:30 -0700

    updated app component to render nav component and added routes to properties.

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,21 @@
 let React = require('react')
 let Popular = require('./Popular')
+let ReactRouter = require ('react-router-dom')
+let Router = ReactRouter.BrowserRouter
+let Route = ReactRouter.Route
+let Nav = require('./Nav')
 
 class App extends React.Component {
     render () {
         return (
+          <Router>
             <div className='container'>
-                <Popular />
+              <Nav />
+              <Route path='/popular' component={Popular} />
             </div>
+          </Router>
         )
     }
 }
 
-module.exports = App;
+module.exports = App
