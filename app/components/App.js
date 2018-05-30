d73fdb765061a001473bf5e1c4620efe071bc2c2
Alex Hidalgo <aj1hidalgo@gmail.com>
2018-05-30 09:53:12 -0700

    added switch to routes.

--- app/components/App.js
+++ app/components/App.js
@@ -3,9 +3,18 @@ let Popular = require('./Popular')
 let ReactRouter = require ('react-router-dom')
 let Router = ReactRouter.BrowserRouter
 let Route = ReactRouter.Route
+let Switch = ReactRouter.Switch
 let Nav = require('./Nav')
 let Home = require('./Home')
 let Battle = require('./Battle')
+
+// switch
+// instead of rendering all the routes it will render only
+// one route at a time
+// show 404 page if the other routes are not hit
+// there is no path so it is active for all other routes
+// need to use exact if you're adding pages to that route otherwise
+// that page will still be active
 
 class App extends React.Component {
     render () {
@@ -13,9 +22,15 @@ class App extends React.Component {
           <Router>
             <div className='container'>
               <Nav />
-              <Route exact path='/' component={Home} />
-              <Route path='/popular' component={Popular} />
-              <Router path='/battle' component={Battle} />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/battle' component={Battle} />
+                <Route exact path='/popular' component={Popular} />
+                <Route render={() => {
+                    return <p>Not Found</p>
+                  }} />
+              </Switch>
+
             </div>
           </Router>
         )
