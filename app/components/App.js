59db5284d16fbe6fb2ff3ed4cbfbe05ca359c73e
Alex Hidalgo <aj1hidalgo@gmail.com>
2018-05-29 22:41:54 -0700

    updated components.

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,8 @@ let ReactRouter = require ('react-router-dom')
 let Router = ReactRouter.BrowserRouter
 let Route = ReactRouter.Route
 let Nav = require('./Nav')
+let Home = require('./Home')
+let Battle = require('./Battle')
 
 class App extends React.Component {
     render () {
@@ -11,7 +13,9 @@ class App extends React.Component {
           <Router>
             <div className='container'>
               <Nav />
+              <Route exact path='/' component={Home} />
               <Route path='/popular' component={Popular} />
+              <Router path='/battle' component={Battle} />
             </div>
           </Router>
         )
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,17 @@
 let React = require('react')
+let Link = require('react-router-dom').Link
 
 function Home () {
-  return (
-    
-  )
-}
+    return (
+      <div className='home-container'>
+        <h1>
+          Github Battle</h1>
+
+        <Link className='button' to='/battle'>
+          Battle
+        </Link>
+      </div>
+    )
+  }
 
 module.exports = Home
