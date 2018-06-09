97c66af525e788dfc4be0a62ea44af49735aa2c1
Suzanne Hidalgo <suzannehidalgo@suzannes-mbp.attlocal.net>
2018-06-08 23:48:01 -0500

    added new components and styles.

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,13 @@
 let React = require('react')
 let Popular = require('./Popular')
-let ReactRouter = require ('react-router-dom')
+let ReactRouter = require('react-router-dom')
 let Router = ReactRouter.BrowserRouter
 let Route = ReactRouter.Route
 let Switch = ReactRouter.Switch
 let Nav = require('./Nav')
 let Home = require('./Home')
 let Battle = require('./Battle')
+let Results = require('./Results')
 
 // switch
 // instead of rendering all the routes it will render only
@@ -17,24 +18,24 @@ let Battle = require('./Battle')
 // that page will still be active
 
 class App extends React.Component {
-    render () {
-        return (
-          <Router>
-            <div className='container'>
-              <Nav />
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/battle' component={Battle} />
-                <Route exact path='/popular' component={Popular} />
-                <Route render={() => {
-                    return <p>Not Found</p>
-                  }} />
-              </Switch>
-
-            </div>
-          </Router>
-        )
-    }
+  render () {
+    return (
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
+            <Route exact path='/popular' component={Popular} />
+            <Route render={() => {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 module.exports = App
--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,13 +1,36 @@
 let React = require('react')
 let PropTypes = require('prop-types')
+let Link = require('react-router-dom').Link
+
+function PlayerPreview (props) {
+  return (
+    <div>
+      <div className='column'>
+        <img className='avatar'
+          src={props.avatar}
+          alt={'Avatar for ' + props.username}
+          />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button className='reset'
+        onClick={props.onReset.bind(null, props.id)}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
 
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      username: ''
-    }
+    this.state = { username: '' }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -75,6 +98,7 @@ class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
     handleSubmit(id, username) {
@@ -86,10 +110,22 @@ class Battle extends React.Component {
       })
     }
 
+    handleReset(id) {
+        this.setState(()=> {
+          let newState = {}
+          newState[id + 'Name'] = ''
+          newState[id + 'Image'] = null
+          return newState
+        })
+    }
+
   // && is a short hand if statement. It requires both statements to be true to render or return
   render() {
     let playerOneName = this.state.playerOneName
     let playerTwoName = this.state.playerTwoName
+    let playerOneImage = this.state.playerOneImage
+    let playerTwoImage = this.state.playerTwoImage
+    let match = this.props.match
 
     return (
       <div>
@@ -100,13 +136,40 @@ class Battle extends React.Component {
             label='Player One'
             onSubmit={this.handleSubmit}
           />}
+
+          {playerOneImage !== null &&
+          <PlayerPreview
+            avatar={playerOneImage}
+            username={playerOneName}
+            onReset={this.handleReset}
+            id='playerOne'
+          />}
+
           {!playerTwoName &&
           <PlayerInput
             id='playerTwo'
             label='Player Two'
             onSubmit={this.handleSubmit}
           />}
+
+          {playerTwoImage !== null &&
+          <PlayerPreview
+            avatar={playerTwoImage}
+            username={playerTwoName}
+            onReset={this.handleReset}
+            id='playerTwo'
+          />}
         </div>
+        {playerOneImage && playerTwoImage &&
+          <Link
+            className='button'
+            to={{
+              pathname: match.url + '/results',
+              search: `?playerOneName=` + playerOneName + '&playerTwoName=' +
+                playerTwoName
+            }}>
+          Battle
+        </Link>}
       </div>
     )
   }
