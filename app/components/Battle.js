16d8834d936f729a8d8c5ed9580d8a07ce17f560
alexhidalgo <suzannehidalgo@suzannes-mbp.attlocal.net>
2018-06-11 00:39:18 -0500

    props.children tutorial

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,50 +1,26 @@
 let React = require('react')
 let PropTypes = require('prop-types')
 let Link = require('react-router-dom').Link
-
-function PlayerPreview (props) {
-  return (
-    <div>
-      <div className='column'>
-        <img className='avatar'
-          src={props.avatar}
-          alt={'Avatar for ' + props.username}
-          />
-        <h2 className='username'>@{props.username}</h2>
-      </div>
-      <button className='reset'
-        onClick={props.onReset.bind(null, props.id)}>
-        Reset
-      </button>
-    </div>
-  )
-}
-
-PlayerPreview.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired
-}
+let PlayerPreview = require('./PlayerPreview')
 
 class PlayerInput extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = { username: '' }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleSubmit(event) {
+  handleSubmit (event) {
     // prevents default behavior of submitting to server
-      event.preventDefault()
+    event.preventDefault()
 
-      this.props.onSubmit(
-        this.props.id,
-        this.state.username
-      )
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    )
   }
-  handleChange(event) {
+  handleChange (event) {
     let value = event.target.value
     this.setState(() => {
       return {
@@ -52,11 +28,11 @@ class PlayerInput extends React.Component {
       }
     })
   }
-// the form has a standard html property called onSubmit which is triggered
-// when the type 'submit' button is triggered. When triggered it calls the function
-// handleSubmit which calls a function that was passed down from the parent
-// called onSubmit. This func updates the state and calls setState.
-  render() {
+  // the form has a standard html property called onSubmit which is triggered
+  // when the type 'submit' button is triggered. When triggered it calls the function
+  // handleSubmit which calls a function that was passed down from the parent
+  // called onSubmit. This func updates the state and calls setState.
+  render () {
     return (
       <form className='column' onSubmit={this.handleSubmit}>
         <label className='header' htmlFor='username'>
@@ -88,7 +64,7 @@ PlayerInput.propTypes = {
 }
 
 class Battle extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       playerOneName: '',
@@ -101,26 +77,26 @@ class Battle extends React.Component {
     this.handleReset = this.handleReset.bind(this)
   }
 
-    handleSubmit(id, username) {
-      this.setState(() => {
-        let newState = {}
-        newState[id + 'Name'] = username
-        newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
-        return newState
-      })
-    }
+  handleSubmit (id, username) {
+    this.setState(() => {
+      let newState = {}
+      newState[id + 'Name'] = username
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
+      return newState
+    })
+  }
 
-    handleReset(id) {
-        this.setState(()=> {
-          let newState = {}
-          newState[id + 'Name'] = ''
-          newState[id + 'Image'] = null
-          return newState
-        })
-    }
+  handleReset (id) {
+    this.setState(() => {
+      let newState = {}
+      newState[id + 'Name'] = ''
+      newState[id + 'Image'] = null
+      return newState
+    })
+  }
 
   // && is a short hand if statement. It requires both statements to be true to render or return
-  render() {
+  render () {
     let playerOneName = this.state.playerOneName
     let playerTwoName = this.state.playerTwoName
     let playerOneImage = this.state.playerOneImage
@@ -131,34 +107,41 @@ class Battle extends React.Component {
       <div>
         <div className='row'>
           {!playerOneName &&
-          <PlayerInput
-            id='playerOne'
-            label='Player One'
-            onSubmit={this.handleSubmit}
-          />}
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit}
+            />}
 
           {playerOneImage !== null &&
-          <PlayerPreview
-            avatar={playerOneImage}
-            username={playerOneName}
-            onReset={this.handleReset}
-            id='playerOne'
-          />}
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}
+            >
+              <button className='reset'
+                onClick={this.handleReset.bind(null, 'playerOne')}>
+                Reset
+              </button>
+            </PlayerPreview>}
 
           {!playerTwoName &&
-          <PlayerInput
-            id='playerTwo'
-            label='Player Two'
-            onSubmit={this.handleSubmit}
-          />}
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
 
           {playerTwoImage !== null &&
-          <PlayerPreview
-            avatar={playerTwoImage}
-            username={playerTwoName}
-            onReset={this.handleReset}
-            id='playerTwo'
-          />}
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+              id='playerTwo'
+            >
+              <button className='reset'
+                onClick={this.handleReset.bind(null, 'playerTwo')}>
+                Reset
+              </button>
+            </PlayerPreview>}
         </div>
         {playerOneImage && playerTwoImage &&
           <Link
@@ -168,8 +151,8 @@ class Battle extends React.Component {
               search: `?playerOneName=` + playerOneName + '&playerTwoName=' +
                 playerTwoName
             }}>
-          Battle
-        </Link>}
+            Battle
+          </Link>}
       </div>
     )
   }
--- app/components/Results.js
+++ app/components/Results.js
@@ -2,6 +2,47 @@ let React = require('react')
 let queryString = require('query-string')
 let api = require('../utils/api')
 let Link = require('react-router-dom').Link
+let PropTypes = require('prop-types')
+let PlayerPreview = require('./PlayerPreview')
+
+function Profile (props) {
+  let info = props.info
+  return (
+    <PlayerPreview
+      avatar={info.avatar_url}
+      username={info.login}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired
+}
+
+function Player (props) {
+  return (
+    <div>
+      <h1 className='header'>{props.label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <Profile info={props.profile} />
+    </div>
+  )
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired
+}
 
 class Results extends React.Component {
   constructor (props) {
@@ -43,7 +84,7 @@ class Results extends React.Component {
     let error = this.state.error
     let winner = this.state.winner
     let loser = this.state.loser
-    let loading = this.state.loading 
+    let loading = this.state.loading
 
     if (loading === true) {
       return <p>Loading...</p>
@@ -58,8 +99,17 @@ class Results extends React.Component {
       )
     }
     return (
-      <div>
-        <div>{JSON.stringify(this.state, null, 2)}</div>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
       </div>
     )
   }
